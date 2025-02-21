6ab012f4eb2c9687d52445792fdb867000d0cd30
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2025-02-20 23:28:21 -0600

    feat: add o3 comment

--- gpu/gemm_gpu.cu
+++ gpu/gemm_gpu.cu
@@ -201,6 +201,8 @@ __global__ void gemm_gpu_o3_kernel(float* A, float* B, float* C, int M, int N,
   // 1. Using 16x16 is faster than 8x8
   // 2. Using 16x16 is faster than 32x32
   // 3. Using 16x32 is slower than 16x16 and 8x8
+  // This means that o2 is the best implementation
+
   // Initialized shared memory array As and Bs to store the sub-matrix of A and
   // B
   __shared__ float As[BLOCK_SIZE_Y][BLOCK_SIZE_X];
