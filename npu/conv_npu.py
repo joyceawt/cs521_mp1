3e3bd0bde3692f5f33e3f9d4e1fd10ae35ed8c26
Joyce Au <122276911+joyceawt@users.noreply.github.com>
2025-02-22 23:51:38 -0600

    fix: attempt to fix the bias slicing

--- npu/conv_npu.py
+++ npu/conv_npu.py
@@ -112,8 +112,8 @@ def conv2d(X, W, bias):
                         psum += nl.matmul(w_tile, window)
 
             # 4) Add bias and store the result in HBM
-            result = psum + bias[oc_tile *
-                                 c_out_pmax: (oc_tile + 1) * c_out_pmax, None, None]
+            bias_slice = bias[oc_tile * c_out_pmax: (oc_tile + 1) * c_out_pmax]
+            result = nisa.tensor_scala(psum, nl.add, bias_slice)
             X_out[b, oc_tile * c_out_pmax: (oc_tile + 1)
                   * c_out_pmax] = nl.store(result)
 
